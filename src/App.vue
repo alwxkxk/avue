b2efe7dba860f0a6319e916e84df51845348e4de
生子当如哈士奇 <592364224@qq.com>
2017-12-17 10:20:41 +0800

    修复背景图无法显示的问题

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <style>
-html,body,#app {height: 100%;margin:0;padding:0;background:#FFF;}
+html,body,#app {height: 100%;margin:0;padding:0;}
 body{
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   background-image: url("./assets/background.jpg");
