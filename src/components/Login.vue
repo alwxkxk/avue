0d6ccf2d4367ba01aab7f653df699739cf6b7b9d
生子当如哈士奇 <592364224@qq.com>
2018-02-01 19:23:46 +0800

    添加管理员账号密码的提示

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -159,6 +159,13 @@
           this.$message.info('取消输入')
         })
       }
+    },
+    mounted: function () {
+      this.$notify({
+        title: '账号密码',
+        message: '管理员账号密码皆为akoa,亦可自行注册普通用户账号',
+        duration: 0
+      })
     }
   }
 </script>
